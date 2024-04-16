cf94d37257092f54e035d6dd9ed3086327267858
Andrej Pecník <andrejpecnik@outlook.sk>
2024-04-16 14:57:10 +0200

    Dorobene texty fotky

--- index.php
+++ index.php
@@ -27,17 +27,17 @@ $spojeni = mysqli_connect($server, $jmeno, $heslo, $databaze);
 					<!-- Logo -->
 						<div id="logo">
 							<span class="image avatar48"><img src="images/avatar.jpg" alt="" /></span>
-							<h1 id="title">Jane Doe</h1>
-							<p>Hyperspace Engineer</p>
+							<h1 id="title">Andrej Pecník</h1>
+							<p>Študent VUT FEKT IBE</p>
 						</div>
 
 					<!-- Nav -->
 						<nav id="nav">
 							<ul>
 								<li><a href="#top" id="top-link"><span class="icon solid fa-home">Intro</span></a></li>
-								<li><a href="#portfolio" id="portfolio-link"><span class="icon solid fa-th">Portfolio</span></a></li>
-								<li><a href="#about" id="about-link"><span class="icon solid fa-user">About Me</span></a></li>
-								<li><a href="#pridajznamku" id="pridajznamku-link"><span class="icon solid fa-database">Pridaj Známku</span></a></li>
+								<li><a href="#portfolio" id="portfolio-link"><span class="icon solid fa-user">Portfólio + O mne</span></a></li>
+								<li><a href="#about" id="about-link"><span class="icon solid fa-database">Databáza známok</span></a></li>
+								<li><a href="#pridajznamku" id="pridajznamku-link"><span class="icon solid fa-plus">Pridaj Známku</span></a></li>
 							</ul>
 						</nav>
 
@@ -47,10 +47,9 @@ $spojeni = mysqli_connect($server, $jmeno, $heslo, $databaze);
 
 					<!-- Social Icons -->
 						<ul class="icons">
-							<li><a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
-							<li><a href="#" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
+							<li><a href="https://www.instagram.com/andrej_pecnik/" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
+							<li><a href="https://www.facebook.com/profile.php?id=100005250886726" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
 							<li><a href="#" class="icon brands fa-github"><span class="label">Github</span></a></li>
-							<li><a href="#" class="icon brands fa-dribbble"><span class="label">Dribbble</span></a></li>
 							<li><a href="#" class="icon solid fa-envelope"><span class="label">Email</span></a></li>
 						</ul>
 
@@ -66,14 +65,13 @@ $spojeni = mysqli_connect($server, $jmeno, $heslo, $databaze);
 						<div class="container">
 
 							<header>
-								<h2 class="alt">Hi! I'm <strong>Prologue</strong>, a <a href="http://html5up.net/license">free</a> responsive<br />
-								site template designed by <a href="http://html5up.net">HTML5 UP</a>.</h2>
-								<p>Ligula scelerisque justo sem accumsan diam quis<br />
-								vitae natoque dictum sollicitudin elementum.</p>
+								<h2 class="alt">Vizitka / Portfólio <strong>Andrej Pecník</strong><br />
+								</h2>
+								<p>stránka bola vytvorená ako projekt na predmet PIS</p>
 							</header>
 
 							<footer>
-								<a href="#portfolio" class="button scrolly">Magna Aliquam</a>
+								<a href="#portfolio" class="button scrolly">Ďalej</a>
 							</footer>
 
 						</div>
@@ -84,54 +82,51 @@ $spojeni = mysqli_connect($server, $jmeno, $heslo, $databaze);
 						<div class="container">
 
 							<header>
-								<h2>Portfolio</h2>
+								<h2>Portfólio + O mne</h2>
 							</header>
 
-							<p>Vitae natoque dictum etiam semper magnis enim feugiat convallis convallis
-							egestas rhoncus ridiculus in quis risus amet curabitur tempor orci penatibus.
-							Tellus erat mauris ipsum fermentum etiam vivamus eget. Nunc nibh morbi quis
-							fusce hendrerit lacus ridiculus.</p>
+							<p>Ako každý mladý človek, mám niekoľko koníčkov. <br>Na nasledujúcich fotkách uvidíte, čomu sa venujem.</p>
 
 							<div class="row">
 								<div class="col-4 col-12-mobile">
 									<article class="item">
-										<a href="#" class="image fit"><img src="images/pic02.jpg" alt="" /></a>
+										<a class="image fit"><img src="images/skialp.jpg" alt="" /></a>
 										<header>
-											<h3>Ipsum Feugiat</h3>
+											<h3>Skialpinizmust</h3>
 										</header>
 									</article>
 									<article class="item">
-										<a href="#" class="image fit"><img src="images/pic03.jpg" alt="" /></a>
+										<a class="image fit"><img src="images/prezentacia.png" alt="" /></a>
 										<header>
-											<h3>Rhoncus Semper</h3>
-										</header>
-									</article>
-								</div>
-								<div class="col-4 col-12-mobile">
-									<article class="item">
-										<a href="#" class="image fit"><img src="images/pic04.jpg" alt="" /></a>
-										<header>
-											<h3>Magna Nullam</h3>
-										</header>
-									</article>
-									<article class="item">
-										<a href="#" class="image fit"><img src="images/pic05.jpg" alt="" /></a>
-										<header>
-											<h3>Natoque Vitae</h3>
+											<h3>Aktivita v projektoch</h3>
 										</header>
 									</article>
 								</div>
 								<div class="col-4 col-12-mobile">
 									<article class="item">
-										<a href="#" class="image fit"><img src="images/pic06.jpg" alt="" /></a>
+										<a class="image fit"><img src="images/pocitace.jpeg" alt="" /></a>
 										<header>
-											<h3>Dolor Penatibus</h3>
+											<h3>Informačné technológie</h3>
 										</header>
 									</article>
 									<article class="item">
-										<a href="#" class="image fit"><img src="images/pic07.jpg" alt="" /></a>
+										<a class="image fit"><img src="images/turistika.jpg" alt="" /></a>
 										<header>
-											<h3>Orci Convallis</h3>
+											<h3>Turistiky</h3>
+										</header>
+									</article>
+								</div>
+								<div class="col-4 col-12-mobile">
+									<article class="item">
+										<a class="image fit"><img src="images/cestovanie.jpg" alt="" /></a>
+										<header>
+											<h3>Cestovanie</h3>
+										</header>
+									</article>
+									<article class="item">
+										<a class="image fit"><img src="images/financie.jpeg" alt="" /></a>
+										<header>
+											<h3>Financie</h3>
 										</header>
 									</article>
 								</div>
@@ -140,15 +135,15 @@ $spojeni = mysqli_connect($server, $jmeno, $heslo, $databaze);
 						</div>
 					</section>
 
-				<!-- About Me -->
+				<!-- Databáza známok -->
 					<section id="about" class="three">
 						<div class="container">
 
 							<header>
-								<h2>About Me</h2>
+								<h2>Databáza známok</h2>
 							</header>
 
-							<a href="#" class="image featured"><img src="images/pic08.jpg" alt="" /></a>
+							<a class="image featured"><img src="images/znamky.jpg" alt="" /></a>
 
 							<a name="tabulkavysledkov"></a>
 							<table>
@@ -194,7 +189,7 @@ $spojeni = mysqli_connect($server, $jmeno, $heslo, $databaze);
 
 							<form method="post" action="ulozznamku.php">
 								<div class="row formznamka">
-									<div class="col-6 col-12-mobile">Dátum:<br><input type="date" name="datum" placeholder="Dátum" /></div>
+									<div class="col-6 col-12-mobile">Dátum:<br><input type="date" name="datum" placeholder="Dátum" class="datuminput"></div>
 								</div>
 								<div class="row formznamka">
 									<div class="col-6 col-12-mobile">
@@ -244,7 +239,7 @@ $spojeni = mysqli_connect($server, $jmeno, $heslo, $databaze);
 
 				<!-- Copyright -->
 					<ul class="copyright">
-						<li>&copy; Untitled. All rights reserved.</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+						<li>&copy; Andrej Pecník. All rights reserved.</li><li>Design: <a href="http://html5up.net">HTML5 UP.</a><br>Personalized by: Andrej Pecník</li>
 					</ul>
 
 			</div>
